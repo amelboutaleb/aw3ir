27d2ca041c760d7d6e46d9240fef3d4da1453c67
amelboutaleb <emilyaamel90@gmail.com>
2025-11-24 10:55:53 +0100

    tp6

--- tp6/meteo-angular/src/app/app-routing-module.ts
+++ tp6/meteo-angular/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Meteo } from './meteo/meteo';
+import { MeteoDetail } from './meteo-detail/meteo-detail';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Meteo },                // page d’accueil
+  { path: 'meteo/:name', component: MeteoDetail }, // détail météo d’une ville
+  { path: '**', redirectTo: '' }                  // redirection si route non reconnue
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
