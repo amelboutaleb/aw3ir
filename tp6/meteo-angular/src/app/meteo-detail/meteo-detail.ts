6e76b13b56913138bf640ea2070f21b19e9247b4
amelboutaleb <emilyaamel90@gmail.com>
2025-11-24 11:55:10 +0100

    tp6

--- tp6/meteo-angular/src/app/meteo-detail/meteo-detail.ts
+++ tp6/meteo-angular/src/app/meteo-detail/meteo-detail.ts
@@ -1,22 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MeteoService } from '../services/meteo';
 
 @Component({
   selector: 'app-meteo-detail',
-  standalone: false,
+  standalone: false, // Garde comme tu l’avais, ou mets à vrai selon ton architecture
   templateUrl: './meteo-detail.html',
-  styleUrl: './meteo-detail.css',
+  styleUrls: ['./meteo-detail.css'],
 })
 export class MeteoDetail implements OnInit {
   cityName: string | null = '';
+  meteo: any = null;
+  latlon: string = '';
+  error: any = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private meteoService: MeteoService
+  ) {}
 
   ngOnInit(): void {
-    // Récupère le paramètre "name" depuis l'URL
     this.cityName = this.route.snapshot.paramMap.get('name');
+    if (this.cityName) {
+      this.getMeteo(this.cityName);
+    }
+  }
+
+  getMeteo(name: string): void {
+    this.meteoService.getMeteo(name)
+      .then(response => {
+        this.meteo = response;
+        if (this.meteo.coord) {
+          this.latlon = `${this.meteo.coord.lat},${this.meteo.coord.lon}`;
+        }
+      })
+      .catch(err => {
+        console.error('Erreur météo :', err);
+        this.error = err;
+      });
   }
 }
+
 
 
 
